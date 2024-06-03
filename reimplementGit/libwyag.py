7badeef2d85071810cae21413280851156ac8593
notaaash <notaaash@gmail.com>
2024-06-03 21:00:17 +0530

    added parsing commits and log command

--- reimplementGit/libwyag.py
+++ reimplementGit/libwyag.py
@@ -351,7 +351,7 @@ def object_hash(fd, fmt, repo=None):
     """Hash object, writing it to repo if provided"""
     data = fd.read()
 
-    #choose contructor according to fmt argument
+    # choose contructor according to fmt argument
     match fmt:
         case b'commit' : obj=GitCommit(data)
         case b'tree' : obj=GitTree(data)
@@ -360,6 +360,144 @@ def object_hash(fd, fmt, repo=None):
         case _: raise Exception("Unknown type %s!" % fmt)
     
     return object_write(obj, repo)
+
+def kvlm_parse(raw, start=0, dct=None):
+    if not dct:
+        dct=collections.OrderedDict()
+        # you CANNOT declare the argument as dct=OrderedDict() or all
+        # call to the functions will endlessly grow the same dict
+    
+    # this function is recursive, it reads a key/value pair, then call
+    # itself back with the new position. So we first need to know
+    # where we are: at a keywords or already in the messageQ
+
+    # we search for the next space and the next newline.
+    spc = raw.find(b' ', start)
+    nl = raw.find(b'\n', start)
+
+    # if space appears before newline, we have a keyword. Otherwise,
+    # it's the final message, which we just read to the end of the file.
+
+    # base case
+    # =========
+    # if newline appears first (or there's no space at all, in which
+    # case find returns -1), we assume a blank line. A blank line
+    # means the remainder of the data is the message. We store it in 
+    # the dictionary, with None as the key, and return.
+    if (spc < 0) or (nl < spc):
+        assert nl == start
+        dct[None] = raw[start+1:]
+        return dct
+    
+    # recursive case
+    # ==============
+    # we read a key-value pair and recurse for the next.
+    key = raw[start:spc]
+
+    # find the end of the value. Continuation lines begin with a 
+    # space, so we loop until we find a "\n" not followed by a space.
+    end = start
+    while True:
+        end = raw.find(b'\n', end+1)
+        if raw[end+1] != ord(' '):
+            break
+
+        # grab the value
+        # also, drop the leading space on continutaion lines
+        value = raw[spc+1:end].replace(b'\n', b'\n')
+
+        # dont overwrite existing data contents
+        if key in dct:
+            if type(dct[key]) == list:
+                dct[key].append(value)
+            else:
+                dct[key] = [ dct[key], value]
+        else:
+            dct[key] = value
+        
+        return kvlm_parse(raw, start=end+1, dct=dct)
+
+def kvlm_serialize(kvlm):
+    ret = b''
+
+    # output fields
+    for k in kvlm.keys():
+        #skip the message itself
+        if k == None:
+            continue
+        val = kvlm[k]
+        #normalize to a list
+        if type(val) != list:
+            val = [ val ]
+        
+        for v in val:
+            ret += k + b' ' + (v.replace(b'\n', b'\n ')) + b'\n'
+
+    # append message
+    ret += b'\n' + kvlm[None] + b'\n'
+
+    return ret
+
+# now we will create the most exciting part!!!! Commit!!!!
+
+class GitCommit(GitObject):
+    fmt = b'commit'
+
+    def deserialize(self, data):
+        self.kvlm = kvlm_parse(data)
+
+    def serialize(self):
+        return kvlm_serialize(self.kvlm)
+    
+    def init(self):
+        self.kvlm = dict()
+
+argsp = argsubparsers.add_parser("log", help="display history of a given commit")
+
+argsp.add_argument("commit",
+                   default="HEAD",
+                   nargs="?",
+                   help="commit to start at.:")
+
+def cmd_log(args):
+    repo = repo_find()
+
+    print("digraph wyaglog{")
+    print(" node[shape=rect]")
+    log_graphviz(repo, object_find(repor, args.commit), set())
+    print("}")
+
+def log_graphviz(repo, sha, seen):
+
+    if sha in seen:
+        return
+    seen.add(sha)
+
+    commit = object_read(repo, sha)
+    short_hash = sha[0:8]
+    message = commit.kvlm[None].decode("utf-8").strip()
+    message = message.replace("\\", "\\\\")
+    message = message.replace("\"", "\\\"")
+
+    if "\n" in message: #keep only the first line
+        message = message[:message.index("\n")]
+    
+    print(" c_{0} [label=\"{1}: {2}\"]".format(sha, sha[0:7], message))
+    assert commit.fmt == b'commit'
+
+    if not b'parent' in commit.kvlm.keys():
+        #this is the base case, the initial commit
+        return
+    
+    parents = commit.kvlm[b'parent']
+
+    if type(parents) != list:
+        parents = [ parents ]
+
+    for p in parents:
+        p = p.decode("ascii")
+        print(" c_{0} -> c{1};".format(sha, p))
+        log_graphviz(repo, p, seen)
 
 
 
